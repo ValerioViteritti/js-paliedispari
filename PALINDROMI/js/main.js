9ee62235c133222c1d969638ce4a5cd31a963ebb
ValerioViteritti <vviteritti@gmail.com>
2024-06-12 19:46:00 +0200

    0.0.5 palindromo finito anche in pagina

--- PALINDROMI/js/main.js
+++ PALINDROMI/js/main.js
@@ -1,32 +1,21 @@
 // Palidroma
-// Chiedere all’utente di inserire una parola
-let parola = prompt("inserisci una parola:")
 
-console.log(parola);
-
-
-
-function Palindromo(parola) {
-
-    // let Palindromo;
-
-    // Confronta la parola con la sua versione invertita
-    return parola === parola.split('').reverse().join('');
-    
-}
 
 // Creare una funzione per capire se la parola inserita è palindroma
-function Verifica() {
-    
-let parola
-let Risultato;
 
-if (Palindromo(parola)) {
-    document.getElementById(Risultato).textContent = `La parola '${parola}' è palindroma`
-    console.log(`La parola '${parola}' è palindroma`);
-} else {
-    document.getElementById(Risultato).textContent = `La parola '${parola}' non è palindroma`
-    console.log(`La parola '${parola}' non è palindroma`);
-}
+// Questo evento si verifica dopo aver caricato l'html
+document.addEventListener("DOMContentLoaded", function() {
+    // Chiedere all’utente di inserire una parola
+    let parola = prompt("inserisci una parola:");
+    // aggancio l'html al risultato che voglio mostrare
+    let risultato = document.getElementById("Risultato");
+    // i due casi della funzione
+    if (Palindromo(parola)) {
+        risultato.textContent = `La parola '${parola}' è palindroma`;
+        console.log(`La parola '${parola}' è palindroma`);
+    } else {
+        risultato.textContent = `La parola '${parola}' non è palindroma`;
+        console.log(`La parola '${parola}' non è palindroma`);
+    }
 
-}
+});
--- PALINDROMI/js/utilities.js
+++ PALINDROMI/js/utilities.js
@@ -1,2 +1,10 @@
 // Funzione per verificare se la parola è palindroma
 
+function Palindromo(parola) {
+
+    // let Palindromo;
+
+    // Confronta la parola con la sua versione invertita
+    return parola === parola.split('').reverse().join('');
+    
+}
